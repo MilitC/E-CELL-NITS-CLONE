0cf01fb8de983ae9f83bb25fdb3a2ee4f9072e77
Milit <136953527+MilitC@users.noreply.github.com>
2025-07-21 16:01:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Events from './components/Events';
+import Team from './components/Team';
+import Sponsors from './components/Sponsors';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Events />
+      <Team />
+      <Sponsors />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
